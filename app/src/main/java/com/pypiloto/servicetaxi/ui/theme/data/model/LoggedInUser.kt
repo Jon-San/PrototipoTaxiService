1796d917f5d022f85a792dedde3ed1c13055336d
Jon-San <jhosallucav@upt.pe>
2024-07-04 02:11:15 -0500

    first commit

---
+++ app/src/main/java/com/pypiloto/servicetaxi/ui/theme/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.pypiloto.servicetaxi.ui.theme.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
